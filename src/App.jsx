feaec81b6b9a935dad8b49fa7bc171e332dcb255
negzowskyy <dominikschinohl@gmail.com>
2026-05-28 14:27:31 +0200

    Still fighting with Discriminator

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
-import { useEffect } from "react";
-import { Connection, PublicKey } from "@solana/web3.js";
-import { BorshAccountsCoder } from "@coral-xyz/anchor";
+  import { useEffect } from "react";
+  import { Connection, PublicKey } from "@solana/web3.js";
+  import { BorshAccountsCoder } from "@coral-xyz/anchor";
 
-import idl from "./klend.json";
+  import idl from "./klend.json";
 
-const connection = new Connection(
-  "https://solana-mainnet.g.alchemy.com/v2/GOelrdYIJDTlUUgy87CXO",
-  "confirmed"
-);
+  const connection = new Connection(
+    "https://solana-mainnet.g.alchemy.com/v2/GOelrdYIJDTlUUgy87CXO",
+    "confirmed"
+  );
 
-const reserve = "9DrvZvyWh1HuAoZxvYWMvkf2XCzryCpGgHqrMjyDWpmo";
-//test high-level scrapa z Kamino
-export default function App() {
-  useEffect(() => {
-    async function run() {
+  const reserve = "9DrvZvyWh1HuAoZxvYWMvkf2XCzryCpGgHqrMjyDWpmo";
+  //test high-level scrapa z Kamino
+  export default function App() {
+    useEffect(() => {
+      async function run() {
 
-      const coder = new BorshAccountsCoder(idl);
+        const coder = new BorshAccountsCoder(idl);
 
-      const account = await connection.getAccountInfo(new PublicKey(reserve));
+        const account = await connection.getAccountInfo(new PublicKey(reserve));
 
-      if (!account) {
-        console.log("no account");
-        return;
-      }else{
-        console.log("account was found correctly");
+        if (!account) {
+          console.log("no account");
+          return;
+        }else{
+          console.log("account was found correctly");
+        }
+        
+        const decoded = coder.decodeUnchecked('Reserve', account.data);
       }
-      
-      const decoded = coder.decode('reserve', account.data.slice(8));
 
-    }
-
-    run();
-  }, []);
+      run();
+    }, []);
 
 
-  return <div></div>;
-}
+    return <div></div>;
+  }
